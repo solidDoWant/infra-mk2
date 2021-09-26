7ef9afbc1838642807b0e652fb2120eb469aab11
solidDoWant <fred.heinecke@yahoo.com>
2021-09-26 10:22:36 -0500

    Tentatively finished flux setup

--- bootstrap/local/install-bootstrap-tools.sh
+++ bootstrap/local/install-bootstrap-tools.sh
@@ -7,6 +7,8 @@ echo "
 Remaining tasks and next steps:
 1. Setup remote hosts - see ../../hardware/README.md for details
 2. Modify the ansible inventory file in the ../remote directory, adding each host to the \"hosts\" group along with any required connection options
-3. Populate the ../remote/vault_password
-4. Run the makefile
+3. Populate the ../remote/vault_password file
+4. Create or import your personal GPG key
+5. Optionally import a Flux GPG key (this will be made automatically if it doesn't exist already)
+6. Run the makefile
 "
